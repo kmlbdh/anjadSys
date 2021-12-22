521f2b06918d00ac19462ef26742176ec897b5a8
Obada <kmlbdh@whatever.com>
2021-12-22 05:55:18 +0200

    change user model schema

--- model/model.user.js
+++ model/model.user.js
@@ -1,19 +1,17 @@
 const { model, Schema} = require("mongoose");
 
 const User = new Schema({
-  fName: {type: String, require: true},
   refId:{type: Number, require: true},
-  nickName:{type: String, default: null},
+  username: {type: String, require: true},
+  nickname:{type: String, default: null},
   password: {type: String, require: true},
   phone: {type: String, require: true},
   tel: {type: String, require: true},
   note: {type: String, require: false, default: null},
-  roles: [
-    {
+  role: {
       type: Schema.Types.ObjectId,
       ref: "Role"
-    }
-  ]
+  }
 });
 
 module.exports = model('User' , User);
