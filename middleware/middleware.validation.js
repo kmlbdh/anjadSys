725e78fd9ea1214608dca255f97ad34b2d3061a0
Obada <kmlbdh@whatever.com>
2021-12-23 23:11:25 +0200

    add validation middleware and schema of joi library

---
+++ middleware/middleware.validation.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+const util = require('util');
+const log = util.debuglog("middleware.validation");
+
+const validation = (schema, property) => {
+  return (req, res, next) => {
+    const { error } = schema.validate(req[property]);
+
+    const valid = error == null;
+
+    if(valid){
+      next();
+    } else {
+      const { details } = error;
+      const message = details.map(i => i.message).join(',');
+
+      log(message);
+      res.status(422).json({message: message});
+    }
+  }
+};
+
+module.exports = validation;
---
+++ schema/schema.validation.js
@@ -0,0 +1,25 @@
+const Joi = require("joi");
+
+module.exports.schema = {
+  signup: Joi.object().keys({
+    username: Joi.string().required().min(3),
+    nickname: Joi.string().min(3).optional(),
+    password: Joi.string().required().label('password'),
+    confirmPassword: Joi.any()
+      .equal(Joi.ref('password'))
+      .required()
+      .strip() //remove it from body after validation
+      .label("Confirm password")
+      .options({ messages: { 'any.only': '{{#label}} doesn\'t match' } }),
+    phone: Joi.number().required().min(9),
+    tel: Joi.number().min(7).optional(),
+    note: Joi.string().min(10).optional(),
+    role: Joi.string().required().min(5),
+  }),
+  singin: Joi.object().keys({
+    username: Joi.string()
+      .pattern(new RegExp('^[AD(-)\d{3})|(AG(-)\d{6})|(C(-)\d{8})]$'))
+      .required(),
+    passowrd: Joi.string().required()
+  }),
+};
