c152a5c280b7fb5bd8c4a60bb089a0187d0edf4e
Obada <kmlbdh@whatever.com>
2021-12-23 23:14:28 +0200

    add validation function as customized for signup to the middleware

--- middleware/middleware.signup.js
+++ middleware/middleware.signup.js
@@ -1,4 +1,6 @@
 const db = require("../model");
+const validation = require("./middleware.validation");
+const { schema } = require("../schema/schema.validation");
 
 const ROLES = db.ROLES;
 const User = db.userModel;
@@ -9,21 +11,23 @@ const checkDuplicateUsernameOrNickname = async(req, res, next) => {
     const user = await User.findOne({$or:[{'username': req.body.username}, {'nickname': req.body.nickname}]});
     if(user){
       fieldName = user.username === req.body.username ? 'username' : 'nickname';
-      return res.status(400).end({message: `${fieldName} is exist!`});
+      return res.status(400).json({message: `${fieldName} is exist!`});
     }
 
     next();
   } catch(error){
-    return res.status(500).end({message: error});
+    return res.status(500).json({message: error});
   }
 };
 
 const checkRoleExisted = async (req, res, next) => {
   if(!req.body.role) return;
   if(!ROLES.includes(req.body.role))
-    return res.status(400).end({message: `Failed! Role ${req.body.role} does not exist!`});
+    return res.status(400).json({message: `Failed! Role ${req.body.role} does not exist!`});
 
   next();
 };
 
-module.exports.verifySingUp = {checkDuplicateUsernameOrNickname, checkRoleExisted};
+const validateSignup = validation(schema.signup, 'body');
+
+module.exports = {checkDuplicateUsernameOrNickname, checkRoleExisted, validateSignup};
