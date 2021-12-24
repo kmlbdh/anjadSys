370f9ede64b00a68499522439105a64c9a67ca8e
Obada <kmlbdh@whatever.com>
2021-12-24 04:31:44 +0200

    add signin, fix  refId issue, import customError class

--- controller/contoller.auth.js
+++ controller/contoller.auth.js
@@ -4,11 +4,12 @@ const db = require("../model");
 const bcrypt = require("bcryptjs");
 const AutoID = require("auto-id-builder");
 const util = require("util");
-const customErorr = require("../classes/customError");
+const customError = require("../classes/customError");
 
 //debugging NOT FOR PRODUCTION 
 const authLog = util.debuglog("controller.auth.js");
 const signupLog = util.debuglog("controller.auth-singup");
+const signinLog = util.debuglog("controller.auth-singin");
 const createUniqueRefIdLog = util.debuglog("controller.auth-createUniqueRefId");
 const getUserLastRefIdLog = util.debuglog("controller.auth-getLastId");
 
@@ -44,13 +45,42 @@ const signup = async (req, res) => {
     res.json({message: "User was registered successfully!"});
   } catch(error) {
     signupLog(error);
-    let messageOfCustomError = error.code === 'INT_ERR' ? error.message : "Failed! User wasn't registered!";
+    let messageOfCustomError = error.code === INTERR ? error.message : "Failed! User wasn't registered!";
     res.json({message: messageOfCustomError });
   }
 };
 
-const singin = () => {
-
+const signin = async(req, res) => {
+  try{
+    const { username, password } = req.body;
+    const user = await User.findOne({refId: username})
+      .populate({
+        path: "role",
+        model: "Role",
+        select: {_id:1, name:1}
+      }).exec();
+    
+    if(!user)
+      throw new customError("User not found!", INTERR);
+    signinLog(user);
+    let validPassword = bcrypt.compareSync(password, user.password);
+    if(!validPassword)
+      throw new customError("wrong password!", INTERR);
+    signinLog(validPassword);
+    const token = jwt.sign({refId: user.refId}, config.secret, {expiresIn: 86400});
+    signinLog(token);
+    res.status(200).json({
+      id: user._id,
+      username: user.username,
+      nickname: user.nickname,
+      role: user.role,
+      accessToken: token 
+    });
+  } catch(error){
+    signinLog(error);
+    let messageOfSignin = error.code === INTERR ? error.message : "Failed! can/'t singIn";
+    res.status(500).json({message: messageOfSignin});
+  }
 };
 
 const createUniqueRefId = async (role) => {
@@ -72,30 +102,30 @@ const createUniqueRefId = async (role) => {
       length = 8;
       break;
   }
-  const lastUserId = await getUserLastRefId(role._id)?.refId;
-  createUniqueRefIdLog(lastUserId);
+  const lastUserId = await getUserLastRefId(role._id);
+  createUniqueRefIdLog(lastUserId?.refId);
   const alphaNumericID = AutoID().newFormat()
     .addPart(true, shortPrefix, 2)
     .addPart(true, '-', 1)
     .addPart(false, 'number', length) 
     .compile();
     
-  let ID = alphaNumericID.generateID(lastUserId);
+  let ID = alphaNumericID.generateID(lastUserId?.refId);
   createUniqueRefIdLog(ID);
   return ID;
 };
 
 const getUserLastRefId = async (roleID) => {
   try{
-    const user = await User.findOne({role: db.mongoose.Types.ObjectId(roleID)}, {}, {sort: {'created_at': -1}})
+    const user = await User.findOne({role: roleID}, {}, {sort: {'created_at': -1}})
       .exec();
       getUserLastRefIdLog(user);
       return user;
   } catch(error){
     getUserLastRefIdLog(error);
-    let messageOfLastID = error.code === 'INT_ERR' ? error.message : "Failed! no id found!";
+    let messageOfLastID = error.code === INTERR ? error.message : "Failed! no id found!";
     res.status(500).json({message: messageOfLastID});
   }
 };
 
-module.exports = {signup, singin};
+module.exports = {signup, signin};
