9eccc644e4e8be8edc46c6f489a26bc3636915c5
Obada <kmlbdh@whatever.com>
2021-12-24 04:32:15 +0200

    add validation for signin at middleware index

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,5 @@
 const verifySingUp = require("./middleware.signup");
+const verifySingIn = require("./middleware.signin");
 const auth = require("./middleware.auth");
 
-module.exports = {verifySingUp, auth};
+module.exports = {verifySingUp, auth, verifySingIn};
