e845781f634f5d026375fe3296e8fe6213ce839f
Obada <kmlbdh@whatever.com>
2022-06-02 11:34:45 +0300

    remove cors from app backend

--- anjadsys-server/app.js
+++ anjadsys-server/app.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const db = require("./models");
 const {main, agent, admin} = require('./routes');
 
@@ -10,15 +9,9 @@ db.sequelize.sync({force: false}).then( () => {
   console.error(err);
 });
 
-const corsOptions = {
-  // origin: "https://injad.albayraq.net"
-  origin: "http://localhost:3033"
-};
-
 const app = express();
 
 app
-  .use(cors(corsOptions))
   .use(express.json())
   .use(express.urlencoded({extended: true}));
 
