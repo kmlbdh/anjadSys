8dd3f100fb204824f04fa5a3686b3f061e921e73
Obada <kmlbdh@whatever.com>
2022-06-04 10:36:42 +0300

    add models manually to backend index page

--- anjadsys-server/models/index.js
+++ anjadsys-server/models/index.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.json')[env];
 const db = {};
 
 let sequelize;
@@ -15,19 +12,30 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+const models = [
+  require('./accident')(sequelize, Sequelize),
+  require('./account')(sequelize, Sequelize),
+  require('./car')(sequelize, Sequelize),
+  require('./carmodel')(sequelize, Sequelize),
+  require('./cartype')(sequelize, Sequelize),
+  require('./insurancepolicy')(sequelize, Sequelize),
+  require('./otherservices')(sequelize, Sequelize),
+  require('./region')(sequelize, Sequelize),
+  require('./role')(sequelize, Sequelize),
+  require('./service')(sequelize, Sequelize),
+  require('./serviceaccident')(sequelize, Sequelize),
+  require('./servicepolicy')(sequelize, Sequelize),
+  require('./user')(sequelize, Sequelize),
+];
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+models.forEach(model => {
+  db[model.name] = model;
+});
+
+models.forEach(model => {
+
+  if (db[model.name].associate) {
+    db[model.name].associate(db);
   }
 });
 
