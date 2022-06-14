223ae84a9e6c1138ad5dd7bb3e5de6c5d2479d75
Obada <kmlbdh@whatever.com>
2022-06-14 03:06:42 +0300

    split the front end app to chunks by lazy loading each major functionality for agent front end

--- anjadsys-client/src/app/modules/core/components/agent/agent-routing.module.ts
+++ anjadsys-client/src/app/modules/core/components/agent/agent-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccidentModule } from './accident/accident.module';
 import { AgentComponent } from './agent.component';
-import { CarModule } from './car/car.module';
-import { InsurancePolicyModule } from './insurance-policy/insurance-policy.module';
 import { MainComponent } from './main/main.component';
-import { ServiceModule } from './service/service.module';
-import { UserModule } from './user/user.module';
-import { AccountModule } from './account/account.module';
 
 const routes: Routes = [
   {
@@ -16,37 +10,49 @@ const routes: Routes = [
     children: [
       {
         path: 'user',
-        loadChildren: () => UserModule,
+        // loadChildren: () => UserModule
+        loadChildren: () => import('./user/user.module')
+        .then(m => m.UserModule),
         data: {breadcrumb: 'المستخدمين'}
       },
       {
         path: 'service',
-        loadChildren: () => ServiceModule,
+        // loadChildren: () => ServiceModule,
+        loadChildren: () => import('./service/service.module')
+        .then(m => m.ServiceModule),
         data: {breadcrumb: 'الخدمات'}
       },
       {
-        path: 'account',
-        loadChildren: () => AccountModule,
-        data: {breadcrumb: 'المالية'}
-      },
-      {
         path: 'car',
-        loadChildren: () => CarModule,
+        // loadChildren: () => CarModule,
+        loadChildren: () => import('./car/car.module')
+        .then(m => m.CarModule),
         data: {breadcrumb: 'السيارات'}
       },
       {
         path: 'accident',
-        loadChildren: () => AccidentModule,
+        // loadChildren: () => AccidentModule,
+        loadChildren: () => import('./accident/accident.module')
+        .then(m => m.AccidentModule),
         data: {breadcrumb: 'الحوادث'}
       },
       {
         path: 'insurance-policy',
-        loadChildren: () => InsurancePolicyModule,
+        // loadChildren: () => InsurancePolicyModule,
+        loadChildren: () => import('./insurance-policy/insurance-policy.module')
+        .then(m => m.InsurancePolicyModule),
         data: {breadcrumb: 'بوالص التأمين'}
       },
       {
+        path: 'account',
+        // loadChildren: () => AccountModule,
+        loadChildren: () => import('./account/account.module')
+        .then(m => m.AccountModule),
+        data: {breadcrumb: 'المالية'}
+      },
+      {
         path: '',
-        component: MainComponent
+        component: MainComponent,
       },
     ]
   }
