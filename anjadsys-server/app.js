98367f020408017c8bd8f97c5b09590615a78cd3
Obada <kmlbdh@whatever.com>
2022-09-11 13:01:17 +0300

    Changes on environment and config URLs.

--- anjadsys-server/app.js
+++ anjadsys-server/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("./models");
-const { adminRouter, agentRouter, loginRouter} = require('./routes');
+const { adminRouter, agentRouter, loginRouter } = require('./routes');
 
 db.sequelize.sync({force: false}).then( () => {
   console.log("Drop and re-sync db");
