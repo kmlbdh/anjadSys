a2b916a69d171a5a5ffd1db54661f909df725a70
Obada <kmlbdh@whatever.com>
2022-10-10 21:14:52 +0300

    add onPush change detection to login page

--- anjadsys-client/src/app/modules/login/login.component.ts
+++ anjadsys-client/src/app/modules/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from './login.service';
@@ -7,7 +7,8 @@ import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnDestroy {
 
